152a24dbce39cabefd75bd48834afdd474e1cb1b
Muhammad Resky Prabowo Sutejo <155411644+Resky89@users.noreply.github.com>
2024-12-05 14:52:24 +0700

    SCRUM - 7 Added userService.js

---
+++ src/services/userService.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview Authentication Service Layer
+ * @description Handles core users business logic and database interactions
+ * @requires ../repositories/userRepository
+ * @requires ../utils/jwtUtils
+ */
+
+import { UserRepository } from '../repositories/userRepository.js';
+
+export class UserService {
+  static async getUser(userId) {
+    try {
+      return await UserRepository.findUser(userId);
+    } catch (error) {
+      throw new Error(`Error getting user: ${error.message}`);
+    }
+  }
+
+  static async updateUser(userId, userData) {
+    try {
+      await UserRepository.findUser(userId);
+      
+      return await UserRepository.update(userId, userData);
+    } catch (error) {
+      throw new Error(`Error updating user: ${error.message}`);
+    }
+  }
+
+  static async deleteUser(userId) {
+    try {
+      await UserRepository.findUser(userId);
+      
+      return await UserRepository.delete(userId);
+    } catch (error) {
+      throw new Error(`Error deleting user: ${error.message}`);
+    }
+  }
+}
